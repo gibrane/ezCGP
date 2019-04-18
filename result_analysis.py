8dce27868c555ccded7509f6d939f0aef03887ee
Johnny Maroun <gmaroun30@gmail.com>
2019-04-18 16:51:19 -0400

    Intermediate Pareto

--- result_analysis.py
+++ result_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from hypervolume import HyperVolume
+from deap import tools
 
 def draw_analysis():
     root_dir = 'outputs_housing'
@@ -36,11 +37,14 @@ def draw_analysis2():
     root_dir = 'outputs_housing'
     file_generation = '{}/generation_number.npy'.format(root_dir)
     generation = np.load(file_generation)
+    print(generation)
     accuracy_score_list = []
     f1_score_list = []
     active_nodes_list = []
     volumes = []
     populations = []
+    pareto1 = []
+    pareto2 = []
     for gen in range(0, generation+1):
         gen_fitnesses = []
         file_pop = '{}/gen{}_pop.npy'.format(root_dir, gen)
@@ -58,6 +62,24 @@ def draw_analysis2():
         active_nodes_list.append(len(active_nodes))
         volumes.append(1- hv.compute(gen_fitnesses))
         populations += list(population)
+        # print(len(population))
+        # nonDom = tools.sortNondominated(population, len(population), first_front_only=False)
+        # print(len(nonDom[0]))
+        # print(nonDom[0])
+        # if (gen == 16):
+    found = False
+    for i in range(0, len(population)):
+        # pareto1.append(population[i].fitness.values[0])
+        # pareto2.append(population[i].fitness.values[1])
+        curr1 = population[i].fitness.values[0]
+        curr2 = population[i].fitness.values[1]
+        for j in range(0, len(population)):
+            if (population[j].fitness.values[0] < curr1 and population[j].fitness.values[1] < curr2):
+                found = True
+        if found == False:
+            pareto1.append(curr1)
+            pareto2.append(curr2)
+        found = False
     plt.subplot(221)
     plt.plot(range(0, generation + 1), accuracy_score_list, linestyle='--', marker='o', color = 'black')
     # plt.legend(['accuracy_score'])
@@ -78,16 +100,24 @@ def draw_analysis2():
     plt.ylabel('HyperVolume')
     plt.xlabel('Generations')
     plt.subplot(224)
-    plt.plot(range(0, generation + 1), f1_score_list, linestyle='--', marker='o', color = 'r')
-    # plt.legend(['active_nodes length'])
-    plt.tight_layout()
-    plt.title('F1-score over generations')
-    plt.ylabel('F1-score')
-    plt.xlabel('Generations')
-    # plt.subplots_adjust(wspace=0.3, hspace=0.3)
+    plt.plot(pareto1, pareto2, linestyle='None', marker='o', color = 'black')
+    # plt.legend(['hyper volume over generations'])
+    plt.title('Pareto Optimality Gen 19')
+    plt.ylabel('APC')
+    plt.xlabel('MAE')
+    # plt.subplot(225)
+    # plt.plot(range(0, generation + 1), f1_score_list, linestyle='--', marker='o', color = 'r')
+    # # plt.legend(['active_nodes length'])
+    # plt.tight_layout()
+    # plt.title('F1-score over generations')
+    # plt.ylabel('F1-score')
+    # plt.xlabel('Generations')
+    plt.subplots_adjust(wspace=0.3, hspace=0.3)
     plt.subplots_adjust(wspace=0.5, hspace=0.5)
     # plt.savefig('{}_saved'.format(root_dir))
+    print(accuracy_score_list, f1_score_list);
     plt.show()
+
 
     # populations = populations
     all_scores = np.array([indiv.fitness.values for indiv in populations])
